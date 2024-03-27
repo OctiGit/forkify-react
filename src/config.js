94f52201e01d67840f7bcc578c4b690eeaf4a632
OctiGit <octi.aicardi@gmail.com>
2024-03-27 16:01:32 +0100

    feat: create config.js for default variables

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "abf65a43-91c3-45e1-8079-eecb2d40bb23";
+export const MODAL_CLOSE_SEC = 2.5;
