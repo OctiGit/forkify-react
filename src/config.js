b902248002f79cdef960804bd4b6d8cd4f253589
OctiGit <octi.aicardi@gmail.com>
2024-04-04 13:07:20 +0200

    feat: add local path for icons

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "abf65a43-91c3-45e1-8079-eecb2d40bb23";
 export const MODAL_CLOSE_SEC = 2.5;
+export const ICONS_PATH = `${process.env.PUBLIC_URL}/img/icons.svg`;
