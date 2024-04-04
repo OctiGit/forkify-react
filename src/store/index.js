b76921020371e1d41f4948d6bc41461d4200f0a7
OctiGit <octi.aicardi@gmail.com>
2024-04-04 13:12:01 +0200

    feat: include every new slice reducer and api reducer to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { bookmarksReducer } from "./slices/bookmarksSlice";
 import { recipeReducer } from "./slices/recipeSlice";
 import { searchReducer } from "./slices/searchSlice";
+import { resultsApi } from "./apis/resultsApi";
 
 export const store = configureStore({
   reducer: {
@@ -11,15 +12,23 @@ export const store = configureStore({
     bookmarks: bookmarksReducer,
     search: searchReducer,
     [recipeApi.reducerPath]: recipeApi.reducer,
+    [resultsApi.reducerPath]: resultsApi.reducer,
   },
   middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware().concat(recipeApi.middleware);
+    return getDefaultMiddleware()
+      .concat(recipeApi.middleware)
+      .concat(resultsApi.middleware);
   },
 });
 
 setupListeners(store.dispatch);
 
 export { useFetchRecipeQuery } from "./apis/recipeApi";
+export { useFetchResultsQuery } from "./apis/resultsApi";
 export { setRecipe, updateServings, setBookmarked } from "./slices/recipeSlice";
 export { addBookmark, deleteBookmark } from "./slices/bookmarksSlice";
-export { setSearchQuery } from "./slices/searchSlice";
+export {
+  setSearchQuery,
+  setSearchResults,
+  setPage,
+} from "./slices/searchSlice";
