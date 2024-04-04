93a7193a971e743507d31065040af9783c18af56
OctiGit <octi.aicardi@gmail.com>
2024-04-04 13:10:07 +0200

    feat: create resultsApi.js

---
+++ src/store/apis/resultsApi.js
@@ -0,0 +1,33 @@
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+import { API_URL, KEY } from "../../config";
+
+const resultsApi = createApi({
+  reducerPath: "resultsApi",
+  baseQuery: fakeBaseQuery(),
+  endpoints(builder) {
+    return {
+      fetchResults: builder.query({
+        async queryFn(query) {
+          try {
+            console.log(query);
+            if (query === "") {
+              return { data: null };
+            } else {
+              const { data } = await axios.get(
+                `${API_URL}?search=${query}&key=${KEY}`
+              );
+              console.log(data);
+              return { data: data.data.recipes };
+            }
+          } catch (error) {
+            return { error };
+          }
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchResultsQuery } = resultsApi;
+export { resultsApi };
