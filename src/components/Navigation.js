47fe7b05ee3060be348903222c695727556a865c
OctiGit <octi.aicardi@gmail.com>
2024-04-21 22:44:06 +0200

    feat: create PreviewView to avoid repeated jsx

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import { ICONS_PATH } from "../config";
+import { setSuccessMessage, toggleAddRecipeWindow } from "../store";
+import PreviewView from "./PreviewView";
 
 function Navigation() {
+  const dispatch = useDispatch();
+  const bookmarks = useSelector((state) => state.bookmarks.data);
+  const { resultId } = useSelector((state) => state.recipe);
+
+  const onClickHandler = () => {
+    dispatch(toggleAddRecipeWindow());
+    dispatch(setSuccessMessage(""));
+  };
   return (
     <nav className="nav">
       <ul className="nav__list">
         <li className="nav__item">
-          <button className="nav__btn nav__btn--add-recipe">
+          <button
+            onClick={onClickHandler}
+            className="nav__btn nav__btn--add-recipe"
+          >
             <svg className="nav__icon">
               <use href={`${ICONS_PATH}#icon-edit`}></use>
             </svg>
@@ -21,14 +35,28 @@ function Navigation() {
           </button>
           <div className="bookmarks">
             <ul className="bookmarks__list">
-              <div className="message">
-                <div>
-                  <svg>
-                    <use href={`${ICONS_PATH}#icon-smile`}></use>
-                  </svg>
+              {bookmarks.length > 0 ? (
+                bookmarks.map(({ id, image_url, title, publisher, key }) => (
+                  <PreviewView
+                    id={id}
+                    active={id === resultId}
+                    image={image_url}
+                    title={title}
+                    publisher={publisher}
+                    key={id}
+                    userGeneratedKey={key}
+                  />
+                ))
+              ) : (
+                <div className="message">
+                  <div>
+                    <svg>
+                      <use href={`${ICONS_PATH}#icon-smile`}></use>
+                    </svg>
+                  </div>
+                  <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
                 </div>
-                <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
-              </div>
+              )}
             </ul>
           </div>
         </li>
---
+++ src/components/PreviewView.js
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
+import { ICONS_PATH } from "../config";
+import { setRecipe, setRecipeId } from "../store";
+
+function PreviewView({
+  id,
+  active,
+  image,
+  title,
+  publisher,
+  userGeneratedKey,
+}) {
+  const dispatch = useDispatch();
+  const bookmarks = useSelector((state) => state.bookmarks.data);
+
+  const onClickHandler = (id) => {
+    const bookmarkedRecipe = bookmarks.find((bookmark) => bookmark.id === id);
+    if (bookmarkedRecipe) dispatch(setRecipe(bookmarkedRecipe));
+    else dispatch(setRecipeId(id));
+  };
+  console.log(userGeneratedKey);
+  return (
+    <li key={id} className="preview">
+      {/* <li className="preview"> */}
+      <a
+        className={`preview__link ${active ? "preview__link--active" : ""}`}
+        href={`#${id}`}
+        onClick={() => onClickHandler(id)}
+      >
+        <figure className="preview__fig">
+          <img src={image} alt={title} />
+        </figure>
+        <div className="preview__data">
+          <h4 className="preview__title">{title}</h4>
+          <p className="preview__publisher">{publisher}</p>
+          <div
+            className={`preview__user-generated ${
+              userGeneratedKey ? "" : "hidden"
+            }`}
+          >
+            <svg>
+              <use href={`${ICONS_PATH}#icon-user`}></use>
+            </svg>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+}
+
+export default PreviewView;
