5d7e79981c4ebff3b20fae6e9169bf609c74bf20
OctiGit <octi.aicardi@gmail.com>
2024-04-30 11:42:15 +0200

    fix: delete recipeApi and resultsApi

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { recipeApi } from "./apis/recipeApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { bookmarksReducer } from "./slices/bookmarksSlice";
 import { recipeReducer } from "./slices/recipeSlice";
 import { searchReducer } from "./slices/searchSlice";
-import { resultsApi } from "./apis/resultsApi";
 import { pageReducer } from "./slices/pageSlice";
 
 export const store = configureStore({
@@ -13,20 +11,11 @@ export const store = configureStore({
     bookmarks: bookmarksReducer,
     search: searchReducer,
     page: pageReducer,
-    [recipeApi.reducerPath]: recipeApi.reducer,
-    [resultsApi.reducerPath]: resultsApi.reducer,
-  },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware()
-      .concat(recipeApi.middleware)
-      .concat(resultsApi.middleware);
   },
 });
 
 setupListeners(store.dispatch);
 
-export { useFetchRecipeQuery, useAddRecipeMutation } from "./apis/recipeApi";
-export { useFetchResultsQuery } from "./apis/resultsApi";
 export {
   setRecipe,
   setRecipeId,
